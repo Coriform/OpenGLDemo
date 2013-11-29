e27ae46b8b37ec5f2b6578d85c9a03f03fc7f206
CoryHunt@gmail.com <CoryHunt@gmail.com@1e61fe72-c765-9c80-2cb5-a0b0b5613cbd>
2013-11-29 07:33:32 +0000

    Cleanup

--- trunk/Roivas/Main.cpp
+++ trunk/Roivas/Main.cpp
@@ -28,8 +28,8 @@ int main(int argc, char** argv)
 	SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER,	1	);
 
 	CoreParams engine_parameters;
-	engine_parameters.window_width	= 1280;
-	engine_parameters.window_height = 720;
+	engine_parameters.window_width	= 896;
+	engine_parameters.window_height = 504;
 	engine_parameters.full_screen	= false;
 	engine_parameters.audio			= false;
 	engine_parameters.borderless	= false;;
